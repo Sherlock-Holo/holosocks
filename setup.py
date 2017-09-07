0e482837e4d23514fc683fdbd4a1d7ca276986ce
Sherlock Holo <sherlockya@gmail.com>
2017-09-07 22:56:38 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Holosocks',
+    version='0.1',
+    author='Sherlock Holo',
+    author_email='sherlockya@gmail.com',
+    license='BSD',
+    keywords='proxy',
+    url='https://github.com/Sherlock-Holo/holosocks',
+    zip_safe=True,
+
+    packages=find_packages(),
+    install_requires=[
+        'setuptools',
+        'pycryptodomex'
+    ],
+    extras_require={
+        'uvloop': []
+    },
+    entry_points={
+        'console_scripts': [
+            'sslocal = sslocal:main',
+            'ssserver = sserver:main'
+        ]
+    },
+
+    classifiers=[
+        'Environment :: Console',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ]
+)
